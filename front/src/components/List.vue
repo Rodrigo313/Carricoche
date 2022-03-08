888124522c5c76393886e1809aaa55fbf2f68913
Rodrigo313 <rodrigopascual1@example.com>
2022-03-08 20:58:32 +0100

    fallo al comprar solucionado

--- front/src/components/List.vue
+++ front/src/components/List.vue
@@ -16,29 +16,29 @@
                 <div>
                     <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Nombre</span>
-              <input type="text" class="form-control" aria-label="Nombre" aria-describedby="basic-addon1">
+              <input type="text" class="form-control" aria-label="Nombre" aria-describedby="basic-addon1" v-model="nombre" @keyup="habilitarBoton(1)" required>
           </div>
 
           <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Apellidos</span>
-              <input type="text" class="form-control" aria-label="Nombre" aria-describedby="basic-addon1">
+              <input type="text" class="form-control" aria-label="Nombre" aria-describedby="basic-addon1" v-model="apellidos" @keyup="habilitarBoton(2)" required>
           </div>
 
           <br>
 
           <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon2">Teléfono</span>
-              <input type="number" max="9" class="form-control" aria-label="Teléfono" aria-describedby="basic-addon2">
+              <input type="text" pattern="[0-9]+" maxlength="9" class="form-control" aria-label="Teléfono" aria-describedby="basic-addon2" v-model="numero" @keyup="habilitarBoton(3)" required>
           </div>
 
 
           <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Correo electrónico" aria-describedby="basic-addon3">
+              <input type="email" class="form-control" placeholder="ejemplo@gmail.com" aria-label="Correo electrónico" aria-describedby="basic-addon3" v-model="correo" @keyup="habilitarBoton(4)" required>
           </div>
 
           <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon2">Dirección</span>
-              <input type="text" class="form-control" aria-label="Dirección" aria-describedby="basic-addon2">
+              <input type="text" class="form-control" aria-label="Dirección" aria-describedby="basic-addon2" v-model="ubicacion" @keyup="habilitarBoton(5)" required>
           </div>
 
           <h4>Opciones de descuento(Elija una opción)</h4>
@@ -60,7 +60,7 @@
           </div>
 
                     <div class="input-group mb-3">
-                        <button type="submit" class="btn btn-primary mb-3" @click="borrar">Comprar</button>
+                        <button id="boton" type="submit" class="btn btn-primary mb-3" @click="borrar" disabled>Comprar</button>
                     </div>
                     <div class="input-group mb-3">
                     <button id="cancelar" type="submit" class="btn btn-danger" @click="cerrarFormulario">Cancelar</button>
@@ -92,7 +92,12 @@ export default{
            familia_numerosa: false,
            precio_final: 0,
            coche: {},
-           id: ""
+           id: "",
+           varNombre: false,
+           varApellidos: false,
+           varTelefono: false,
+           varCorreo: false,
+           varDireccion: false
        }
    },
    created(){
@@ -146,6 +151,50 @@ export default{
         check: function(e) {
             this.calcularDescuento();
             this.unaOpcion();
+        },
+        habilitarBoton(index){
+            switch(index){
+            case 1:
+                if(this.nombre == ""){
+                    this.varNombre = false;
+                }else{
+                    this.varNombre = true;
+                }
+            break;
+            case 2:
+                if(this.apellidos == ""){
+                    this.varApellidos = false;
+                }else{
+                    this.varApellidos = true;
+                }
+            break;
+            case 3:
+                if(this.numero == ""){
+                    this.varTelefono = false;
+                }else if(this.numero.length == 9){
+                    this.varTelefono = true;
+                }
+            break;
+            case 4:
+                if(this.correo == ""){
+                    this.varCorreo = false;
+                }else if(this.correo.includes("@")){
+                    this.varCorreo = true;
+                }
+            break;
+            case 5:
+                if(this.ubicacion == ""){
+                    this.varDireccion = false;
+                }else{
+                    this.varDireccion = true;
+                }
+            break;
+      }
+      if(this.varNombre && this.varApellidos && this.varTelefono && this.varCorreo && this.varDireccion){
+        document.getElementById("boton").disabled = false;
+      }else{
+        document.getElementById("boton").disabled = true;
+      }
         }
    },
 }
